93c090d58f1d36fdbf090cf95945cdf5617c8d3a
AYOUB9060 <ayoubeladib906@gmail.com>
2023-09-11 18:12:44 -0700

    finding the second biggest number from the command line arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-
-const numbers = process.argv.slice(2).map(Number);
-
-if (numbers.length <= 2) {
-  console.log(0); // not enough numbers to find the second largest
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  const largest = Math.max(...numbers); // Find the largest and second largest numbers in the array
-  numbers.splice(numbers.indexOf(largest), 1);
-  if (numbers.length === 1) {
-    console.log(numbers[0]); // If there's only one number left, print it
-  } else {
-    const secondLargest = Math.max(...numbers); // Find the second largest number
-    console.log(secondLargest);
-  }
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
